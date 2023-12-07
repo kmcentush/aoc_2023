c1f470bfe4e1440460426d036b7739baf364efbf
Kyle McEntush <kyle.mcentush@cuberg.net>
2023-12-06 17:40:21 -0800

    puzzle 2

---
+++ src/aoc/day1/puzzle2.py
@@ -0,0 +1,59 @@
+import re
+
+from aoc.utils import multiline_input
+
+# Specify digits
+DIGIT_MAP = {
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
+DIGITS = list(DIGIT_MAP.keys()) + list("123456789")
+DIGITS_PTRNS = [re.compile(v) for v in DIGITS]
+
+
+def _str_to_digits(line: str) -> str:
+    # Find indices of matches
+    matches = {}
+    for ptrn in DIGITS_PTRNS:
+        for match in ptrn.finditer(line):
+            matches[match.start()] = match.group()
+
+    # Reconstruct input
+    out = ""
+    matches = dict(sorted(matches.items()))
+    for val in matches.values():
+        if val in DIGIT_MAP.keys():
+            out += DIGIT_MAP[val]
+        else:
+            out += val
+
+    return out
+
+
+def extract_numbers(puzzle: str) -> list[int]:
+    numbers = []
+    puzzle = puzzle.strip()
+    for line in puzzle.splitlines():
+        numbers_str = _str_to_digits(line)
+        number = int(numbers_str[0] + numbers_str[-1])
+        numbers.append(number)
+    return numbers
+
+
+def solve_puzzle(puzzle: str) -> int:
+    numbers = extract_numbers(puzzle)
+    answer = sum(numbers)
+    print(f"Answer: {answer}")
+    return answer
+
+
+if __name__ == "__main__":  # pragma: no cover
+    puzzle = multiline_input()
+    solve_puzzle(puzzle)
--- tests/tests_aoc/test_day1.py
+++ tests/tests_aoc/test_day1.py
@@ -1,4 +1,5 @@
-from aoc.day1.puzzle1 import extract_numbers, solve_puzzle
+from aoc.day1 import puzzle1 as p1
+from aoc.day1 import puzzle2 as p2
 
 
 def test_puzzle1():
@@ -11,7 +12,30 @@ def test_puzzle1():
     """
 
     # Assert
-    numbers = extract_numbers(puzzle)
+    numbers = p1.extract_numbers(puzzle)
     assert numbers == [12, 38, 15, 77]
-    answer = solve_puzzle(puzzle)
+    answer = p1.solve_puzzle(puzzle)
     assert answer == 142
+
+
+def test_puzzle2():
+    # Assert helpers
+    digits_str = p2._str_to_digits("eightwo1three4")
+    assert digits_str == "82134"
+
+    # Define puzzle
+    puzzle = """
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+    """
+
+    # Assert
+    numbers = p2.extract_numbers(puzzle)
+    assert numbers == [29, 83, 13, 24, 42, 14, 76]
+    answer = p2.solve_puzzle(puzzle)
+    assert answer == 281
