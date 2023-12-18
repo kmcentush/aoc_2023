5fde29d936c71c6500e9fe7dce05cc076d094b94
Kyle McEntush <kdmcentush@gmail.com>
2023-12-17 17:11:27 -0800

    day 13 puzzle 2

--- src/aoc/day13/puzzle.py
+++ src/aoc/day13/puzzle.py
@@ -33,7 +33,12 @@ def parse_puzzle(puzzle: str) -> list[tuple[list[str], list[str]]]:
     return games
 
 
-def _find_mirror(vals: list[str]) -> int:
+def _maybe_smudge(line1: str, line2: str) -> bool:
+    diff = [line1[i] != line2[i] for i in range(len(line1))]
+    return sum(diff) == 1
+
+
+def _find_mirror(vals: list[str], orig_idx: int) -> int:
     # Find entries to check
     num_vals = len(vals)
     to_check = []
@@ -41,10 +46,17 @@ def _find_mirror(vals: list[str]) -> int:
         to_check.append((i, i + 1))
 
     # Check entries
+    allow_smudge = orig_idx != -1
     for first_idx, second_idx in to_check:
+        # Allow one smudge
+        did_smudge = False
+
         # Ensure we start from symmetry
         if vals[first_idx] != vals[second_idx]:
-            continue
+            if allow_smudge and not did_smudge and _maybe_smudge(vals[first_idx], vals[second_idx]):
+                did_smudge = True
+            else:
+                continue
 
         # Explore outward
         before_idx = first_idx - 1
@@ -52,29 +64,48 @@ def _find_mirror(vals: list[str]) -> int:
         is_mirror = True
         while before_idx >= 0 and after_idx < num_vals:
             if vals[before_idx] != vals[after_idx]:
-                is_mirror = False
-                break
+                if allow_smudge and not did_smudge and _maybe_smudge(vals[before_idx], vals[after_idx]):
+                    did_smudge = True
+                else:
+                    is_mirror = False
+                    break
             before_idx -= 1
             after_idx += 1
-        if is_mirror:
+        if is_mirror and second_idx != orig_idx:
             return second_idx  # number of entries before symmetry
 
     return 0
 
 
-def find_mirrors(games: list[tuple[list[str], list[str]]]) -> tuple[list[int], list[int]]:
+def find_mirrors(
+    games: list[tuple[list[str], list[str]]],
+    allow_smudge: bool,
+    orig_row_mirrors: list[int] = [],
+    orig_col_mirrors: list[int] = [],
+) -> tuple[list[int], list[int]]:
     row_mirrors = []
     col_mirrors = []
-    for rows, cols in games:
-        row_mirrors.append(_find_mirror(rows))
-        col_mirrors.append(_find_mirror(cols))
+    for i, (rows, cols) in enumerate(games):
+        orig_row_idx = orig_row_mirrors[i] if allow_smudge else -1
+        row_mirrors.append(_find_mirror(rows, orig_row_idx))
+        orig_col_idx = orig_col_mirrors[i] if allow_smudge else -1
+        col_mirrors.append(_find_mirror(cols, orig_col_idx))
     return row_mirrors, col_mirrors
 
 
 def solve_puzzle1(puzzle: str) -> int:
     games = parse_puzzle(puzzle)
-    row_mirrors, col_mirrors = find_mirrors(games)
+    row_mirrors, col_mirrors = find_mirrors(games, False)
     answer = sum(row_mirrors) * 100 + sum(col_mirrors)
+    print(f"Answer: {answer}")
+    return answer
+
+
+def solve_puzzle2(puzzle: str) -> int:
+    games = parse_puzzle(puzzle)
+    row_mirrors, col_mirrors = find_mirrors(games, False)
+    row_mirrors_smudge, col_mirrors_smudge = find_mirrors(games, True, row_mirrors, col_mirrors)
+    answer = sum(row_mirrors_smudge) * 100 + sum(col_mirrors_smudge)
     print(f"Answer: {answer}")
     return answer
 
@@ -82,4 +113,4 @@ def solve_puzzle1(puzzle: str) -> int:
 if __name__ == "__main__":  # pragma: no cover
     puzzle = load_puzzle("puzzle.txt")
     assert solve_puzzle1(puzzle) == 33047
-    # assert solve_puzzle2(puzzle) == ?
+    assert solve_puzzle2(puzzle) == 28806
--- tests/tests_aoc/test_day13.py
+++ tests/tests_aoc/test_day13.py
@@ -50,7 +50,7 @@ def test_puzzle():
     assert len(games) == 5
     assert len(games[0][0]) == 7  # game 1 rows
     assert len(games[0][1]) == 9  # game 1 columns
-    row_mirrors, col_mirrors = p.find_mirrors(games)
+    row_mirrors, col_mirrors = p.find_mirrors(games, False)
     assert row_mirrors == [0, 4, 0, 3, 0]
     assert col_mirrors == [5, 0, 4, 0, 0]
 
@@ -59,5 +59,5 @@ def test_puzzle():
     assert answer1 == 709
 
     # Assert puzzle 2
-    # answer2 = p.solve_puzzle2(puzzle)
-    # assert answer2 == 1400
+    answer2 = p.solve_puzzle2(puzzle)
+    assert answer2 == 1400
